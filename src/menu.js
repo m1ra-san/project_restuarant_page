c2ae51fa9ccffde2eb764ec82569f84a71a9a327
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-08-29 11:25:12 +0800

    Add new menu

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
-    
+
 function loadMenu() {
     const container = document.querySelector('#content');
-    container.innerHTML=""
+    container.innerHTML = ""
     const title = document.createElement("h1");
     title.textContent = "ASIAN FOREST MEALS 101";
 
@@ -9,10 +9,20 @@ function loadMenu() {
     des.textContent = "This meals are things that make you like a warrior and look effortlesly fit"
 
 
-    
+    const subtitle = document.createElement("h2");
+    subtitle.textContent = "Breakfast";
+
+    const pande = document.createElement("h3");
+    pande.textContent = "Pandesal";
+
+    const pandedes = document.createElement("p");
+    pandedes.textContent = "A cheap Life saver. Even one can last you till dinner"
 
     container.appendChild(title)
     container.appendChild(des)
+    container.appendChild(subtitle)
+    container.appendChild(pande)
+    container.appendChild(pandedes)
 
 }
 
