458d768989475bcba4692cb1fa4549c82ee56d63
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-09-16 12:57:36 +0800

    Implement css style

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 console.log("Hello");
 
+import "./style.css";
+import home from "./home.js";
+import menu from "./menu.js";
+import contact from "./contact.js";
 
-import home from './home.js';
-import menu from './menu.js';
-import contact from './contact.js';
+const buttons = document.querySelectorAll("button");
+home();
 
-
-
-const buttons=document.querySelectorAll('button')
-home()
-
-
-
-buttons.forEach(button=>{
-    button.addEventListener("click",e=>{
-        if(e.target.id==='home'){
-            home()
-        }else if(e.target.id==='menu'){
-            menu()
-        }else if(e.target.id==='contact'){
-            contact()
-        }
-    })
-})
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.id === "home") {
+      home();
+    } else if (e.target.id === "menu") {
+      menu();
+    } else if (e.target.id === "contact") {
+      contact();
+    }
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
+// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/template.html"],
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    
+  },
 };
